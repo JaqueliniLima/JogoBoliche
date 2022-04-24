08bde5b9d2ec229654ba25fd2fb54c59b66a2b2b
JaqueliniLima <jaquelini.lima.info@gmail.com>
2022-04-24 18:06:43 -0300

    Código estruturado

---
+++ src/boliche/Jogador.java
@@ -0,0 +1,53 @@
+package boliche;
+
+public class Jogador {
+	private final int id;
+	private final String nome;
+
+	public Jogador(int id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Jogador other = (Jogador) obj;
+		if (id != other.id)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return nome;
+	}
+	
+}
---
+++ src/boliche/Main.java
@@ -0,0 +1,9 @@
+package boliche;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Boliche boliche = new Boliche();
+		boliche.iniciarJogo();
+	}
+}
